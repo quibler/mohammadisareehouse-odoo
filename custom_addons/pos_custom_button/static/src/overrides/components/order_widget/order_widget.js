de9ea7305562428dd3e858f18e4a7ce3a8c2d15b
quibler <ashwinidroid@gmail.com>
2025-04-20 23:23:01 +0530

    fixed all templates

---
+++ custom_addons/pos_custom_button/static/src/overrides/components/order_widget/order_widget.js
@@ -0,0 +1,11 @@
+/** @odoo-module */
+
+import { OrderWidget } from "@point_of_sale/app/generic_components/order_widget/order_widget";
+import { patch } from "@web/core/utils/patch";
+
+patch(OrderWidget, {
+    props: {
+        ...OrderWidget.props,
+        sales_person: { type: String, optional: true },
+    }
+});
