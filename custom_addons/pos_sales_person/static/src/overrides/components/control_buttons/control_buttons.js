d9ee1865a1f545c5f6e2dc69dfef093067b8cf98
quibler <ashwinidroid@gmail.com>
2025-04-21 00:29:13 +0530

    name changes

--- custom_addons/pos_sales_person/static/src/overrides/components/control_buttons/control_buttons.js
+++ custom_addons/pos_sales_person/static/src/overrides/components/control_buttons/control_buttons.js
@@ -1,6 +1,6 @@
 import {patch} from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
-import { SalesPersonButton } from "@pos_custom_button/app/screens/product_screen/control_buttons/sales_person_button/sales_person_button";
+import { SalesPersonButton } from "@pos_sales_person/static/src/app/screens/product_screen/control_buttons/sales_person_button/sales_person_button";
 
 console.log("!!!!!!!", SalesPersonButton)
 patch(ControlButtons, {
