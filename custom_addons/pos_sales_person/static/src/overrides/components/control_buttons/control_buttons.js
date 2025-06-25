6706c3dc8b66b4d627715f80b203025882608270
quibler <ashwinidroid@gmail.com>
2025-06-25 17:38:08 +0530

    added amount discount button to POS

--- custom_addons/pos_sales_person/static/src/overrides/components/control_buttons/control_buttons.js
+++ custom_addons/pos_sales_person/static/src/overrides/components/control_buttons/control_buttons.js
@@ -3,10 +3,12 @@
 import { patch } from "@web/core/utils/patch";
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
 import { SalesPersonButton } from "@pos_sales_person/app/screens/product_screen/control_buttons/sales_person_button/sales_person_button";
+import { DiscountButton } from "@pos_sales_person/app/screens/product_screen/control_buttons/discount_button/discount_button";
 
 patch(ControlButtons, {
     components: {
         ...ControlButtons.components,
         SalesPersonButton,
+        DiscountButton,
     },
 });
