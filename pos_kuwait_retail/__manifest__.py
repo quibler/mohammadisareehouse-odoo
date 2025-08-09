bb574f3c8aaa9c17f25d6bfecd71f09f60cea067
quibler <ashwinidroid@gmail.com>
2025-08-09 17:28:34 +0530

    made changes to order receipt to fix error. changed version in manifest for force refresh

--- pos_kuwait_retail/__manifest__.py
+++ pos_kuwait_retail/__manifest__.py
@@ -58,7 +58,7 @@
         """,
     'author': 'Ashwini Kumar',
     'category': 'Point Of Sale',
-    'version': '18.0.5.0.0',  # Version bump for custom price feature
+    'version': '18.0.5.1.0',  # Version bump to fix XPath receipt template errors
     'depends': [
         'point_of_sale',
         'pos_hr',
