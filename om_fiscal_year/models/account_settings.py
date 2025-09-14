780befffbc4c2eb46530132ff7dff4551a404689
quibler <ashwinidroid@gmail.com>
2025-09-14 09:24:59 +0530

    fixing server logs warning issues

--- om_fiscal_year/models/account_settings.py
+++ om_fiscal_year/models/account_settings.py
@@ -11,15 +11,19 @@ class ResConfigSettings(models.TransientModel):
         related='company_id.fiscalyear_last_month', readonly=False
     )
     tax_lock_date = fields.Date(
+        string="Tax Hard Lock Date",  # This won't break anything
         related='company_id.hard_lock_date', readonly=False
     )
     sale_lock_date = fields.Date(
+        string="Sales Hard Lock Date",  # Safe to add
         related='company_id.hard_lock_date', readonly=False
     )
     purchase_lock_date = fields.Date(
+        string="Purchase Hard Lock Date",  # Safe to add
         related='company_id.hard_lock_date', readonly=False
     )
     hard_lock_date = fields.Date(
+        string="General Hard Lock Date",  # Safe to add
         related='company_id.hard_lock_date', readonly=False
     )
     fiscalyear_lock_date = fields.Date(
